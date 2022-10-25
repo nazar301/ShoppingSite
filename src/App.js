d19e76b7c0445a6028f5fb5d79f840d0bfd50ed8
nazar301 <nazar.basistyy@gmail.com>
2022-10-25 10:37:18 -0400

    Routing addition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Store from './pages/Store'
 import Success from './pages/Success'
 import Cancel from './pages/Cancel'
-import './App.css';
+import './App.css'
 import {Container} from 'react-bootstrap'
 import NavBarComponent from './components/NavBarComponent'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
